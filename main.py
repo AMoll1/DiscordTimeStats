6abfc702f18f9bd78da931e0027c131c5b729144
kevindietrichstein <43498723+kevindietrichstein@users.noreply.github.com>
2018-09-22 23:44:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import datetime
 import os
 
 ranks = collections.defaultdict(dict)
-start_time = collections.defaultdict(dict)
-
+start = collections.defaultdict(dict)
 client = discord.Client()
 
 
@@ -17,9 +16,9 @@ async def on_ready():
     for member in client.get_all_members():
         if not member.bot:
             if str(member.status) != "offline":
-                start_time[member.id]["Online"] = datetime.datetime.now()
+                start[member.id]["Online"] = datetime.datetime.now()
             if member.game:
-                start_time[member.id][str(member.game)] = datetime.datetime.now()
+                start[member.id][str(member.game)] = datetime.datetime.now()
 
 
 @client.event
@@ -29,14 +28,14 @@ async def on_member_update(before, after):
         if before.status != after.status:
             # if online
             if str(before.status) == "offline":
-                start_time[after.id]["Online"] = datetime.datetime.now()
+                start[after.id]["Online"] = datetime.datetime.now()
             # if offline from online
             elif str(after.status) == "offline":
                 try:
                     end_time = datetime.datetime.now()
-                    total_time = end_time - start_time[after.id].get("Online", 0)
+                    total_time = end_time - start[after.id].get("Online", 0)
                     ranks[after.id]["Online"] = ranks[after.id].get("Online", 0) + int(total_time.total_seconds())
-                    start_time[after.id]["Online"] = 0
+                    start[after.id]["Online"] = 0
                 except TypeError:
                     pass
         # if game changed
@@ -45,56 +44,59 @@ async def on_member_update(before, after):
             if before.game and after.game:
                 try:
                     end_time = datetime.datetime.now()
-                    total_time = end_time - start_time[after.id].get(str(before.game), 0)
+                    total_time = end_time - start[after.id].get(str(before.game), 0)
                     ranks[after.id][str(before.game)] = (ranks[after.id].get(str(before.game), 0)
                                                          + int(total_time.total_seconds()))
-                    start_time[after.id][str(before.game)] = 0
+                    start[after.id][str(before.game)] = 0
                 except TypeError:
                     pass
                 finally:
-                    start_time[after.id][str(after.game)] = datetime.datetime.now()
+                    start[after.id][str(after.game)] = datetime.datetime.now()
             # if game starts
             elif after.game:
-                start_time[after.id][str(after.game)] = datetime.datetime.now()
+                start[after.id][str(after.game)] = datetime.datetime.now()
             # if game ends
             elif before.game:
                 try:
                     end_time = datetime.datetime.now()
-                    total_time = end_time - start_time[after.id].get(str(before.game), 0)
+                    total_time = end_time - start[after.id].get(str(before.game), 0)
                     ranks[after.id][str(before.game)] = (ranks[after.id].get(str(before.game), 0)
                                                          + int(total_time.total_seconds()))
-                    start_time[after.id][str(before.game)] = 0
+                    start[after.id][str(before.game)] = 0
                 except TypeError:
                     pass
 
 
 @client.event
 async def on_message(message):
+    # save times from current status
+    for member in start.items():
+        for status, time in member[1].items():
+            if time != 0:
+                end_time = datetime.datetime.now()
+                total_time = end_time - start[member[0]].get(status, 0)
+                ranks[member[0]][status] = (ranks[member[0]].get(status, 0) + int(total_time.total_seconds()))
+                start[member[0]][status] = datetime.datetime.now()
+    sorted_ranks = sorted(ranks.items(), key=lambda x: x[1].get("Online", 0), reverse=True)
     # message evaluation
     msg = tuple(message.content.split())
+    out = str()
+    # if cmd help
     if "nlb.help" in msg and len(msg) == 1:
-        await client.send_message(message.channel, "command:\t'rank [amount=5]'")
+        await client.send_message(message.channel, "command:\t'rank [amount=5]'\t- gets ranks up to amount\n" +
+                                                   "\t" * 6 + "'my.rank'" + "\t" * 5 + " - gets own stats")
+    # if cmd rank
     elif "rank" in msg and len(msg) <= 2:
         amount = 5
-        if msg[1].isdigit():
+        if len(msg) == 2 and msg[1].isdigit():
             amount = int(msg[1])
-        # save times from current status
-        for member in start_time.items():
-            for status, time in member[1].items():
-                if time != 0:
-                    end_time = datetime.datetime.now()
-                    total_time = end_time - start_time[member[0]].get(status, 0)
-                    ranks[member[0]][status] = (ranks[member[0]].get(status, 0) + int(total_time.total_seconds()))
-                    start_time[member[0]][status] = datetime.datetime.now()
-        # print member rank
-        sorted_ranks = sorted(ranks.items(), key=lambda x: x[1].get("Online", 0), reverse=True)
+        # get embed
         i = 1
         for member in sorted_ranks:
             # for member on this server
             if message.server.get_member(member[0]):
                 if i <= amount:
-                    embed = discord.Embed(title="{}\t{}".format(i, message.server.get_member(member[0]).display_name),
-                                          color=discord.Colour.red())
+                    out += "\n\n**{}.\t{}**\n".format(i, message.server.get_member(member[0]).display_name)
                     for j, (status, time) in enumerate(sorted(member[1].items(), key=lambda x: x[1], reverse=True), 1):
                         if j <= 5:
                             hours = 0
@@ -104,12 +106,28 @@ async def on_message(message):
                                 minutes, seconds = divmod(seconds, 60)
                             if minutes >= 60:
                                 hours, minutes = divmod(minutes, 60)
-                            embed.add_field(name=status,
-                                            value="{}:{}:{}"
-                                            .format(str(hours).zfill(2), str(minutes).zfill(2), str(seconds).zfill(2)),
-                                            inline=False)
-                    await client.send_message(message.channel, embed=embed)
+                            out += ("\n{}:{}:{}\t**{}**"
+                                    .format(str(hours).zfill(2), str(minutes).zfill(2), str(seconds).zfill(2), status))
                     i += 1
+        # print embed
+        embed = discord.Embed(title="TOP {}".format(amount), description=out, color=discord.Colour.red())
+        await client.send_message(message.channel, embed=embed)
+    # if cmd my rank
+    elif "my.rank" in msg and len(msg) == 1:
+        # get embed
+        for status, time in ranks.get(message.author.id).items():
+            hours = 0
+            minutes = 0
+            seconds = time
+            if seconds >= 60:
+                minutes, seconds = divmod(seconds, 60)
+            if minutes >= 60:
+                hours, minutes = divmod(minutes, 60)
+            out += ("\n{}:{}:{}\t**{}**"
+                    .format(str(hours).zfill(2), str(minutes).zfill(2), str(seconds).zfill(2), status))
+        # print embed
+        embed = discord.Embed(title="{}".format(message.author.display_name), description=out, color=discord.Colour.red())
+        await client.send_message(message.channel, embed=embed)
 
 
 client.run(os.getenv("TOKEN"))
