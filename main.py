b15f6ec301ffe3e552f5b21e72697134cf9dd7e1
kevindietrichstein <43498723+kevindietrichstein@users.noreply.github.com>
2018-09-23 15:54:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 
 ranks = collections.defaultdict(dict)
 start = collections.defaultdict(dict)
+
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="nlb.help//Stalking {} users".
+    await client.change_presence(game=discord.Game(name="ts.help//Stalking {} users".
                                                    format(len(tuple(client.get_all_members())))))
     # if online or in-game from beginning
     for member in client.get_all_members():
@@ -69,65 +70,73 @@ async def on_member_update(before, after):
 
 @client.event
 async def on_message(message):
-    # save times from current status
-    for member in start.items():
-        for status, time in member[1].items():
-            if time != 0:
-                end_time = datetime.datetime.now()
-                total_time = end_time - start[member[0]].get(status, 0)
-                ranks[member[0]][status] = (ranks[member[0]].get(status, 0) + int(total_time.total_seconds()))
-                start[member[0]][status] = datetime.datetime.now()
-    sorted_ranks = sorted(ranks.items(), key=lambda x: x[1].get("Online", 0), reverse=True)
-    # message evaluation
-    msg = tuple(message.content.split())
-    out = str()
-    # if cmd help
-    if "nlb.help" in msg and len(msg) == 1:
-        await client.send_message(message.channel, "command:\t'rank [amount=5]'\t- gets ranks up to amount\n" +
-                                                   "\t" * 6 + "'my.rank'" + "\t" * 5 + " - gets own stats")
-    # if cmd rank
-    elif "rank" in msg and len(msg) <= 2:
-        amount = 5
-        if len(msg) == 2 and msg[1].isdigit():
-            amount = int(msg[1])
-        # get embed
-        i = 1
-        for member in sorted_ranks:
-            # for member on this server
-            if message.server.get_member(member[0]):
-                if i <= amount:
-                    out += "\n\n**{}.\t{}**\n".format(i, message.server.get_member(member[0]).display_name)
-                    for j, (status, time) in enumerate(sorted(member[1].items(), key=lambda x: x[1], reverse=True), 1):
-                        if j <= 5:
-                            hours = 0
-                            minutes = 0
-                            seconds = time
-                            if seconds >= 60:
-                                minutes, seconds = divmod(seconds, 60)
-                            if minutes >= 60:
-                                hours, minutes = divmod(minutes, 60)
-                            out += ("\n{}:{}:{}\t**{}**"
-                                    .format(str(hours).zfill(2), str(minutes).zfill(2), str(seconds).zfill(2), status))
-                    i += 1
-        # print embed
-        embed = discord.Embed(title="TOP {}".format(amount), description=out, color=discord.Colour.red())
-        await client.send_message(message.channel, embed=embed)
-    # if cmd my rank
-    elif "my.rank" in msg and len(msg) == 1:
-        # get embed
-        for status, time in ranks.get(message.author.id).items():
-            hours = 0
-            minutes = 0
-            seconds = time
-            if seconds >= 60:
-                minutes, seconds = divmod(seconds, 60)
-            if minutes >= 60:
-                hours, minutes = divmod(minutes, 60)
-            out += ("\n{}:{}:{}\t**{}**"
-                    .format(str(hours).zfill(2), str(minutes).zfill(2), str(seconds).zfill(2), status))
-        # print embed
-        embed = discord.Embed(title="{}".format(message.author.display_name), description=out, color=discord.Colour.red())
-        await client.send_message(message.channel, embed=embed)
+    if message.content.startswith("ts."):
+        msg = message.content.replace("ts.", "", 1)
+        print(msg)
+        if len(msg.split()) == 1:
+            # save times from current status
+            for member in start.items():
+                for status, time in member[1].items():
+                    if time != 0:
+                        end_time = datetime.datetime.now()
+                        total_time = end_time - start[member[0]].get(status, 0)
+                        ranks[member[0]][status] = (ranks[member[0]].get(status, 0) + int(total_time.total_seconds()))
+                        start[member[0]][status] = datetime.datetime.now()
+            # message evaluation
+            out = str()
+            # if cmd help
+            if "help" in msg:
+                await client.send_message(message.channel,
+                                          "command:\t'ts.top'" + "\t" * 5 + "  - gets time stats of top 5 members\n" +
+                                          "\t" * 6 + "'ts.top[amount]'" + "\t- gets time stats of top amount members\n" +
+                                          "\t" * 6 + "'ts.self' " + "\t" * 5 + " - gets own time stats")
+            # if cmd rank
+            elif "top" in msg:
+                amount = 5
+                msg = msg.replace("top", "", 1)
+                if msg.isdigit():
+                    amount = int(msg)
+                # get embed
+                sorted_ranks = sorted(ranks.items(), key=lambda x: x[1].get("Online", 0), reverse=True)
+                i = 1
+                for member in sorted_ranks:
+                    # for member on this server
+                    if message.server.get_member(member[0]):
+                        if i <= amount:
+                            out += "\n\n**{}.\t{}**\n".format(i, message.server.get_member(member[0]).display_name)
+                            for j, (status, time) in enumerate(sorted(member[1].items(), key=lambda x: x[1],
+                                                                      reverse=True), 1):
+                                if j <= 5:
+                                    hours = 0
+                                    minutes = 0
+                                    seconds = time
+                                    if seconds >= 60:
+                                        minutes, seconds = divmod(seconds, 60)
+                                    if minutes >= 60:
+                                        hours, minutes = divmod(minutes, 60)
+                                    out += ("\n{}:{}:{}\t**{}**".format(str(hours).zfill(2), str(minutes).zfill(2),
+                                                                        str(seconds).zfill(2), status))
+                            i += 1
+                # print embed
+                embed = discord.Embed(title="TOP {}".format(amount), description=out, color=discord.Colour.red())
+                await client.send_message(message.channel, embed=embed)
+            # if cmd my rank
+            elif "self" in msg:
+                # get embed
+                for status, time in sorted(ranks.get(message.author.id).items(), key=lambda x: x[1], reverse=True):
+                    hours = 0
+                    minutes = 0
+                    seconds = time
+                    if seconds >= 60:
+                        minutes, seconds = divmod(seconds, 60)
+                    if minutes >= 60:
+                        hours, minutes = divmod(minutes, 60)
+                    out += ("\n{}:{}:{}\t**{}**".format(str(hours).zfill(2), str(minutes).zfill(2),
+                                                        str(seconds).zfill(2), status))
+                # print embed
+                embed = discord.Embed(title="{}".format(message.author.display_name), description=out,
+                                      color=discord.Colour.red())
+                await client.send_message(message.channel, embed=embed)
 
 
 client.run(os.getenv("TOKEN"))
